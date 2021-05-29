985f67fadf5a890d8180005ef273e95f26d0a34e
Ph1Doc <oleg.sapun@gmail.com>
2021-05-29 23:03:11 +0300

    feat: add new functional for HappyArray

--- src/main/kotlin/subtask1/HappyArray.kt
+++ src/main/kotlin/subtask1/HappyArray.kt
@@ -4,6 +4,21 @@ class HappyArray {
 
     // TODO: Complete the following function
     fun convertToHappy(sadArray: IntArray): IntArray {
-        throw NotImplementedError("Not implemented")
+        return checkList(sadArray.toMutableList()).toIntArray()
+    }
+
+    private fun checkList(sadList:  MutableList<Int>):  MutableList<Int> {
+        var oneMoreTimeCheck = false
+        val sadListCopy = sadList.toMutableList()
+        for (i in 1..sadList.size - 2) if (sadList[i-1].toString().toInt() + sadList[i+1].toString().toInt() < sadList[i].toString().toInt() && !oneMoreTimeCheck) {
+            val removeAt = sadListCopy.removeAt(i)
+            oneMoreTimeCheck = true
+            break
+        }
+        return if (oneMoreTimeCheck) {
+            checkList(sadListCopy)
+        } else {
+            sadListCopy
+        }
     }
 }
