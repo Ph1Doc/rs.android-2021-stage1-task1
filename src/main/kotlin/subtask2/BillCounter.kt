35d316b77516f3e5f166d0d7ba6ab366141c0e86
Ph1Doc <oleg.sapun@gmail.com>
2021-05-30 12:18:49 +0300

    feat: add new functional for Bill Counter

--- src/main/kotlin/subtask2/BillCounter.kt
+++ src/main/kotlin/subtask2/BillCounter.kt
@@ -5,6 +5,22 @@ class BillCounter {
     // TODO: Complete the following function
     // The output could be "Bon Appetit" or the string with number(e.g "10")
     fun calculateFairlySplit(bill: IntArray, k: Int, b: Int): String {
-        throw NotImplementedError("Not implemented")
+        var output = "Bon Appetit"
+        var sum = 0
+        var costOfBrainMeals = 0
+
+        for (i in bill.indices) if (i != k) {
+            sum += bill[i]
+        } else {
+            costOfBrainMeals += bill[i]
+            print("costOfBrainMeals = " + costOfBrainMeals + "\n")
+        }
+
+        sum /= 2
+        if (b != sum) {
+            output = (b - sum).toString()
+        }
+
+        return output
     }
 }
