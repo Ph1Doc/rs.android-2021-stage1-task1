10bf6e7034a1d12575e09906cdeba925fb2e4be8
Ph1Doc <oleg.sapun@gmail.com>
2021-05-30 15:32:40 +0300

    fear: add new functional for String Parser

--- src/main/kotlin/subtask3/StringParser.kt
+++ src/main/kotlin/subtask3/StringParser.kt
@@ -2,8 +2,91 @@ package subtask3
 
 class StringParser {
 
+    private val openBracket = "(";
+    private val closeBracket = ")";
+    private val openStaple = "[";
+    private val closeStaple = "]";
+    private val openBtk = "<";
+    private val closeBtk = ">";
+
     // TODO: Complete the following function
     fun getResult(inputString: String): Array<String> {
-        throw NotImplementedError("Not implemented")
+        val outputlist = mutableListOf<String>()
+
+        val listOfBracket = listOf<String>(openBracket, openBtk, openStaple)
+        for (i in inputString.indices) {
+            for (j in listOfBracket) {
+                if (inputString[i].toString() == j) {
+                    outputlist.add(makeString(inputString.substring(i, inputString.length), j))
+                }
+            }
+        }
+    return outputlist.toTypedArray()
+    }
+
+    private fun makeString(inputString: String, bracket: String): String {
+        if (bracket == openBracket) {
+            val index = inputString.indexOf(closeBracket)
+            var result = ""
+            if (index != -1) {
+                var count = 0
+                for (i in inputString.indices) {
+                    if (inputString[i].toString() == openBracket) {
+                        count = count + 1
+                    }
+                    if (inputString[i].toString() == closeBracket) {
+                        count = count - 1
+                        if (count == 0) {
+                            result = inputString.substring(1, i)
+                            break
+                        }
+                    }
+                }
+                return result
+            }
+        }
+
+        if (bracket == openStaple) {
+            val index = inputString.indexOf(closeStaple)
+            var result = ""
+            if (index != -1) {
+                var count = 0
+                for (i in inputString.indices) {
+                    if (inputString[i].toString() == openStaple) {
+                        count = count + 1
+                    }
+                    if (inputString[i].toString() == closeStaple) {
+                        count = count - 1
+                        if (count == 0) {
+                            result = inputString.substring(1, i)
+                            break
+                        }
+                    }
+                }
+                return result
+            }
+        }
+
+        if (bracket == openBtk) {
+            val index = inputString.indexOf(closeBtk)
+            var result = ""
+            if (index != -1) {
+                var count = 0
+                for (i in inputString.indices) {
+                    if (inputString[i].toString() == openBtk) {
+                        count = count + 1
+                    }
+                    if (inputString[i].toString() == closeBtk) {
+                        count = count - 1
+                        if (count == 0) {
+                            result = inputString.substring(1, i)
+                            break
+                        }
+                    }
+                }
+                return result
+            }
+        }
+        return "Error \n"
     }
 }
